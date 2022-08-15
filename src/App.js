5c938f403473b99a6a636c6a811e04390124afd0
Jimmy Joel <jimmyjoel177@gmail.com>
2022-08-15 04:12:06 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import React from 'react';
+import {Routes, Route} from "react-router-dom"
+import Navbar from "./components/Navbar"
+import Details from "./components/Details"
+import Cart from "./components/Cart/Cart"
+import Default from "./components/Default"
+import ProductList from "./components/ProductList"
+import Modal from './components/Modal';
+
+
+function App() {
+  return (
+    <React.Fragment>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<ProductList/>}/>
+        <Route path="/details" element={<Details/>}/>
+        <Route path="/cart" element={<Cart/>}></Route>
+        <Route path="*" element={<Default/>}></Route>
+      </Routes>
+      <Modal></Modal>
+    </React.Fragment>
+  );
+}
+
+export default App;
